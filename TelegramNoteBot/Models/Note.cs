1ec01046e7fb86035a282a0a1f1b59efea81a701
Di_St <dima.stozhok335@gmail.com>
2025-07-26 01:06:47 +0300

    Exstension model

--- TelegramNoteBot/Models/Note.cs
+++ TelegramNoteBot/Models/Note.cs
@@ -4,7 +4,11 @@ public class Note
 {
     public int Id { get; set; }
     public long UserId { get; set; }
-    public string Text { get; set; }= string.Empty;
+
+    public string UserName { get; set; } = string.Empty;
+    
+    public string Title { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
     
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
