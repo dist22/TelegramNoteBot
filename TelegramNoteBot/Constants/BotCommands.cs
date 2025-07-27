70dbd707855b55c18d51db7de109054e054cd6ea
Di_St <dima.stozhok335@gmail.com>
2025-07-27 03:02:02 +0300

    Create constants instead of using strin

---
+++ TelegramNoteBot/Constants/BotCommands.cs
@@ -0,0 +1,11 @@
+namespace TelegramNoteBot.Constants;
+
+public static class BotCommands
+{
+    public const string AddNote = "➕ Add note";
+    public const string MyNotes = "📒 My notes";
+    public const string DeleteNote = "🗑 Delete note";
+    public const string SearchNote = "🔍 Search Notes";
+    public const string Start = "/start";
+    public const string AboutDeveloper = "About developer";
+}
---
+++ TelegramNoteBot/Constants/CallBackCommands.cs
@@ -0,0 +1,7 @@
+namespace TelegramNoteBot.Constants;
+
+public static class CallBackCommands
+{
+    public const string Delete = "del_";
+    public const string Info = "inf_";
+}
