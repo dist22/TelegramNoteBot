67845e83a0712c423eabe20dbda2ba749e658709
Di_St <dima.stozhok335@gmail.com>
2025-07-31 23:54:59 +0300

    Added markup for tags

--- TelegramNoteBot/Bot/ReplyMarkupBuilder.cs
+++ TelegramNoteBot/Bot/ReplyMarkupBuilder.cs
@@ -20,15 +20,22 @@ public static class ReplyMarkupBuilder
             [new KeyboardButton(BotTagCommands.RemoveTags), new KeyboardButton(BotTagCommands.Back)]
         ]) { ResizeKeyboard = true };
     
-    public static InlineKeyboardMarkup NotesMarkup(IEnumerable<Note> notes, string emoji, string callBackData) 
+    public static InlineKeyboardMarkup NotesMarkup(IEnumerable<Note> notes, string emoji, string callBackCommand) 
         => new (notes.Select(n => new[]
             {
-                InlineKeyboardButton.WithCallbackData($"{emoji} {n.Title}", $"{callBackData}{n.Id}")
+                InlineKeyboardButton.WithCallbackData($"{emoji} {n.Title}", $"{callBackCommand}{n.Id}")
             })
             .ToArray()
         );
 
+    public static InlineKeyboardMarkup TagMarkup(IEnumerable<Tag> tags, string emoji, string callBackCommand)
+        => new(tags.Select(t => new[]
+            {
+                InlineKeyboardButton.WithCallbackData($"{emoji} {t.Name}",$"{callBackCommand}{t.Id}]")
+            })
+            .ToArray()
+        );
     public static InlineKeyboardButton AboutDeveloper()
-        => new ("       My GitHub       ", "https://github.com/dist22");
+        => new ("My GitHub", "https://github.com/dist22");
 
 }
