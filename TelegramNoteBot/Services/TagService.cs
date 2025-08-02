3d288d03006256c4abe4db200f03e534d33cd1b1
Di_St <dima.stozhok335@gmail.com>
2025-08-02 15:58:34 +0300

    Added "IfExist" method for tag

--- TelegramNoteBot/Services/TagService.cs
+++ TelegramNoteBot/Services/TagService.cs
@@ -8,23 +8,34 @@ public class TagService(AppDbContext dbContext, ILogger<TagService> logger)
 {
     private readonly DbSet<Tag> _dbSet = dbContext.Tag;
 
-    public async Task AddTag(string name)
+    public async Task AddTag(string name, long userId)
     {
-        await _dbSet.AddAsync(new Tag { Name = name });
+        await _dbSet.AddAsync(new Tag 
+            { 
+                Name = name, 
+                AuthorId = userId 
+            });
         await dbContext.SaveChangesAsync();
     }
 
-    public async Task<List<Tag>>GetAllAsync() 
-        => await _dbSet.ToListAsync();
+    public async Task<List<Tag>> GetAllAsync(long userId)
+        => await _dbSet
+            .Where(t => t.AuthorId == userId)
+            .ToListAsync();
 
-    public async Task<bool> DeleteTag(int tagId)
+    public async Task<bool> TagExist(string name, long userId)
+        => await _dbSet
+            .Where(t => t.AuthorId == userId)
+            .AnyAsync(t => t.Name == name);
+
+    public async Task<bool> DeleteTag(int tagId, long userId)
     {
-        var tag = await _dbSet.FirstOrDefaultAsync(n => n.Id == tagId);
+        var tag = await _dbSet
+            .Where(t => t.AuthorId == userId)
+            .FirstOrDefaultAsync(n => n.Id == tagId);
         if (tag == null) return false;
         _dbSet.Remove(tag);
         await dbContext.SaveChangesAsync();
         return true;
     }
-
-
 }
