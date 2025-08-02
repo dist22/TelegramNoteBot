2d71b343f87ab13a4a8ed162b8d898891038d4b1
Di_St <dima.stozhok335@gmail.com>
2025-08-02 15:57:10 +0300

    Cosmetic adjustments have been made

--- TelegramNoteBot/Bot/TagCommandHandler.cs
+++ TelegramNoteBot/Bot/TagCommandHandler.cs
@@ -1,5 +1,6 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using TelegramNoteBot.Constants;
 using TelegramNoteBot.Enums;
 using TelegramNoteBot.Models;
@@ -9,50 +10,75 @@ namespace TelegramNoteBot.Bot;
 
 public class TagCommandHandler(TagService tagService, UserSessionService userSessionService)
 {
-    public async Task HandleCommandAsync(ITelegramBotClient client,long chatId,string text, User user, CancellationToken cts)
+    public async Task HandleCommandAsync(ITelegramBotClient client, long chatId, string text, User user,
+        CancellationToken cts)
     {
         var state = userSessionService.GetOrCreate(user.Id);
-        var tags = await tagService.GetAllAsync();
+        var tags = await tagService.GetAllAsync(user.Id);
 
         switch (text)
         {
             case BotTagCommands.AddTags:
                 state.State = BotUserState.AddingTag;
-                await client.SendMessage(chatId, "Enter tag name, he must started with #", cancellationToken: cts);
+                await client.SendMessage(chatId, "<b>✍️ Enter tag name</b>\nIt must start with <code>#</code>"
+                    , ParseMode.Html, cancellationToken: cts);
                 break;
+
             case BotTagCommands.Tags:
-                var mes =  tags.Any() ? string.Join(", ", tags.Select(t => t.Name)) : "😕 You don't have any tags yet.";
-                await client.SendMessage(chatId, mes, cancellationToken: cts);
+                var mes = tags.Any()
+                    ? string.Join(", ", tags.Select(t => t.Name))
+                    : "😕 <i>You don't have any tags yet.</i>";
+                await client.SendMessage(chatId, $"<b>🏷 Your tags:</b>\n\n{mes}", ParseMode.Html,
+                    cancellationToken: cts);
                 break;
+
             case BotTagCommands.RemoveTags:
                 if (!tags.Any())
                 {
-                    await client.SendMessage(chatId,"<UNK> You don't have any tags yet.", cancellationToken: cts);
+                    await client.SendMessage(chatId, "😕 <i>You don't have any tags yet.</i>", ParseMode.Html,
+                        cancellationToken: cts);
+                    return;
                 }
-                var mesText = "You Tags";
-                await client.SendMessage(chatId, mesText,
-                    replyMarkup: ReplyMarkupBuilder.TagMarkup(tags, BotCommandEmojis.X, CallBackCommands.TagDelete), cancellationToken: cts);
+
+                await client.SendMessage(chatId, "<b>🗑 Select tag to remove:</b>",
+                    replyMarkup: ReplyMarkupBuilder.TagMarkup(tags, BotCommandEmojis.X, CallBackCommands.TagDelete),
+                    parseMode: ParseMode.Html, cancellationToken: cts);
                 break;
+
             case BotTagCommands.Back:
-                await client.SendMessage(chatId, "🔙 Returned to main menu", replyMarkup: ReplyMarkupBuilder.MainMenu(), cancellationToken: cts);
+                await client.SendMessage(chatId, "<b>🔙 Returned to main menu</b>",
+                    replyMarkup: ReplyMarkupBuilder.MainMenu(), parseMode: ParseMode.Html, cancellationToken: cts);
                 state.State = BotUserState.None;
                 break;
         }
-
     }
 
-    public async Task HandleTextTagInputAsync(ITelegramBotClient client, string text, long chatId, User user, UserNoteState state, CancellationToken cts)
+    public async Task HandleTextTagInputAsync(ITelegramBotClient client, string text, long chatId, User user,
+        UserNoteState state, CancellationToken cts)
     {
         switch (state.State)
         {
             case BotUserState.AddingTag:
                 if (!text.StartsWith("#"))
                 {
-                    await client.SendMessage(chatId, "Uncorrected tag, try again", cancellationToken: cts);
-                    break;
+                    await client.SendMessage(chatId,
+                        "❌ <b>Invalid tag</b>. It must start with <code>#</code>. Try again.", ParseMode.Html,
+                        cancellationToken: cts);
+                    return;
                 }
-                await tagService.AddTag(text);
-                await client.SendMessage(chatId, "Tag added", cancellationToken: cts);
+
+                if (await tagService.TagExist(text, user.Id))
+                {
+                    await client.SendMessage(chatId,
+                        "⚠️ <i>This tag already exists.</i> Try again.", ParseMode.Html,
+                        cancellationToken: cts);
+                    return;
+                }
+
+                await tagService.AddTag(text, user.Id);
+                await client.SendMessage(chatId, "<b>Tag added ✅</b>", ParseMode.Html,
+                    replyMarkup: ReplyMarkupBuilder.TagManagementMenu()
+                    , cancellationToken: cts);
                 state.State = BotUserState.TagManagement;
                 break;
         }
