76ca8d6d85b5263a1a346a137e5f0ee93e907173
Di_St <dima.stozhok335@gmail.com>
2025-08-02 15:58:53 +0300

    Added authorId for tag

--- TelegramNoteBot/Models/Tag.cs
+++ TelegramNoteBot/Models/Tag.cs
@@ -4,6 +4,7 @@ public class Tag
 {
     
     public int Id { get; set; }
+    public long AuthorId { get; set; }
     public string Name { get; set; } = string.Empty;
     public List<NoteTag> NoteTags { get; set; } = new();
 
