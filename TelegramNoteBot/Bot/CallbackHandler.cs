98328623e5ed86a740ead161e7d87f48cf19bac4
Di_St <dima.stozhok335@gmail.com>
2025-08-03 23:47:59 +0300

    Added tag callback support

--- TelegramNoteBot/Bot/CallbackHandler.cs
+++ TelegramNoteBot/Bot/CallbackHandler.cs
@@ -1,42 +1,110 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using TelegramNoteBot.Constants;
 using TelegramNoteBot.Enums;
 using TelegramNoteBot.Services;
 
 namespace TelegramNoteBot.Bot;
 
-public class CallbackHandler(NoteService noteService, UserSessionService userSessionService, TagService tagService)
+public class CallbackHandler(
+    NoteService noteService,
+    UserSessionService userSessionService,
+    TagService tagService,
+    NoteTagService noteTagService)
 {
     public async Task HandleUpdateAsync(ITelegramBotClient client, CallbackQuery callbackQuery, CancellationToken cts)
     {
         var chatId = callbackQuery.Message.Chat.Id;
         var messageId = callbackQuery.Message.MessageId;
         var user = callbackQuery.From;
-        var data = callbackQuery.Data;
+
         var state = userSessionService.GetOrCreate(user.Id);
-        var d = 1;
-        
-        if(data.StartsWith(CallBackCommands.Delete) && int.TryParse(data[4..], out var noteIdToDelete))
+
+        var parts = callbackQuery.Data?.Split("|");
+        var command = parts?.ElementAtOrDefault(0);
+        var idStr = parts?.ElementAtOrDefault(1);
+
+        switch (command)
         {
-            var result = await noteService.DeleteNote(user.Id, noteIdToDelete);
-            if (result)
-            {
-                await client.EditMessageText(chatId, messageId, "Note deleted", cancellationToken: cts);
-            }
-            else await client.SendMessage(chatId, "Can`t delete the note 😢", cancellationToken: cts);
-        }
-        else if (data.StartsWith(CallBackCommands.Info) && int.TryParse(data[4..], out var noteIdToShow))
-        {
-            var note = await noteService.GetNote(user.Id, noteIdToShow);
-            await client.EditMessageText(chatId, messageId, note.Text, cancellationToken: cts);
-        }
-        else if (data.StartsWith(CallBackCommands.TagDelete) && int.TryParse(data[5..], out var tagIdToDelete))
-        {
-            var tag = await tagService.DeleteTag(tagIdToDelete, user.Id);
-            var message = tag ? "Tag successfully deleted" : "Tag deleted";
-            await client.EditMessageText(chatId, messageId, message, cancellationToken: cts);
+            case CallBackCommands.Delete:
+                if (int.TryParse(idStr, out var noteIdToDelete))
+                {
+                    var response = await noteService.DeleteNote(user.Id, noteIdToDelete)
+                        ? "<b>✅ Note deleted successfully.</b>"
+                        : "<b>❌ Failed to delete the note.</b>";
+                    await client.EditMessageText(chatId, messageId, response, ParseMode.Html, cancellationToken: cts);
+                }
+
+                break;
+
+            case CallBackCommands.Info:
+                if (int.TryParse(idStr, out var noteIdToShow))
+                {
+                    var note = await noteService.GetNote(user.Id, noteIdToShow);
+                    
+                    var tags = note?.NoteTags
+                        .Select(n => n.Tag.Name)
+                        .ToList();
+                    
+                    var formattedTags = tags is { Count: > 0 }
+                        ? $"🏷 <i>Tags:</i> {string.Join(", ", tags)}"
+                        : "🏷 <i>Tags:</i> <code>None</code>";
+                    
+                    var formattedNote = $"""
+                                         <b>📝 {note.Title}</b>
+
+                                         <i>{note.Text}</i>
+
+                                         <i>🗓 Created: {note.CreatedAt:dd MMM yyyy, HH:mm}</i>
+                                         
+                                         {formattedTags}
+                                         """;
+                    await client.EditMessageText(chatId, messageId, formattedNote, ParseMode.Html,
+                        cancellationToken: cts);
+                }
+
+                break;
+
+            case CallBackCommands.TagDelete:
+                if (int.TryParse(idStr, out var tagIdToDelete))
+                {
+                    var response = await tagService.DeleteTag(tagIdToDelete, user.Id)
+                        ? "<b>✅ Tag successfully deleted.</b>"
+                        : "<b>❌ Failed to delete tag.</b>";
+                    await client.EditMessageText(chatId, messageId, response, ParseMode.Html, cancellationToken: cts);
+                }
+
+                break;
+
+            case CallBackCommands.SelectTag:
+                if (int.TryParse(idStr, out var tagIdToAdd))
+                {
+                    var tag = await tagService.GetTagAsync(user.Id, tagIdToAdd);
+                    if (tag == null) return;
+
+                    if (state.SelectedTags.All(t => t.Id != tag.Id))
+                        state.SelectedTags.Push(tag);
+
+                    if (state.SelectedTags.Count == 3)
+                    {
+                        await noteTagService.AddNoteTagAsync(state.LastAddedNoteId, state.SelectedTags.Peek().Id);
+                        await client.SendMessage(chatId, "<b>✅ Max 3 tags selected. Saving note...</b>",
+                            ParseMode.Html, replyMarkup: ReplyMarkupBuilder.MainMenu(), cancellationToken: cts);
+                        userSessionService.Clear(user.Id);
+                    }
+                    else
+                    {
+                        var remaining = 3 - state.SelectedTags.Count;
+
+                        await noteTagService.AddNoteTagAsync(state.LastAddedNoteId, state.SelectedTags.Peek().Id);
+
+                        await client.SendMessage(chatId, $"✅ Tag added. You can select {remaining} more.",
+                            ParseMode.Html, cancellationToken: cts);
+                    }
+                }
+
+                break;
         }
     }
-    
 }
