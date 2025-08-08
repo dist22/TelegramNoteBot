17715f102b0567be2e814ffd7abade5243bcdd98
Di_St <dima.stozhok335@gmail.com>
2025-08-08 23:56:01 +0300

    Added Redis database for cache contains

---
+++ TelegramNoteBot/Services/RedisCallBackStorage.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+using StackExchange.Redis;
+using TelegramNoteBot.Models;
+
+namespace TelegramNoteBot.Services;
+
+public class RedisCallBackStorage(IConnectionMultiplexer redis)
+{
+    private readonly IDatabase _db = redis.GetDatabase();
+
+    public async Task<string> StoreCallBackAsync(NoteCallBackData callBackData)
+    {
+        var key = $"{callBackData.User.Id}:{Guid.NewGuid()}";
+        var json = JsonSerializer.Serialize(callBackData);
+        await _db.StringSetAsync(key, json, TimeSpan.FromMinutes(10));
+        return key;
+    }
+
+    public async Task<NoteCallBackData?> GetCallBackAsync(string key)
+    {
+        var json = await _db.StringGetAsync(key);
+        if (json.IsNullOrEmpty) return null;
+        return JsonSerializer.Deserialize<NoteCallBackData>(json!);
+    }
+}
